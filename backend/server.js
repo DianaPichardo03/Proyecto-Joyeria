e683f7e50cec849df40cc2d583b1127d5d5b1523
Diana Pichardo <dianapigd94@gmail.com>
2026-05-31 18:49:44 -0600

    server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
-
-require("dotenv").config();
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");

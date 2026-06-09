27b5902c71673557e30ab57db13218a62765abd8
Diana Pichardo <dianapigd94@gmail.com>
2026-06-09 01:11:05 -0600

    debug cloudinary

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 const API_URL =
-process.env.REACT_APP_API_URL || "http://localhost:3001";
+process.env.REACT_APP_API_URL || 
+"https://proyecto-joyeria-50z0.onrender.com";
 
 export default API_URL;
